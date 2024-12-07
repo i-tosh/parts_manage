594abae1864a7bc360203ce67d59d81a05f9fb9b
i-tosh <tosh.ishida@gmail.com>
2024-12-07 18:19:13 +0900

    tab追加

--- parts_manage.py
+++ parts_manage.py
@@ -6,37 +6,49 @@ import datetime
 
 st.title("製品管理システム")
 
-df = pd.read_csv("data.csv")
+tab1, tab2, tab3 = st.tabs(["受注", "発注","棚卸"])
 
-parts_no = df["製品番号"].unique().tolist()
-
-select = st.selectbox("部品番号", parts_no)
-
-st.write('選択された製品は',select)
-
-d = st.date_input(
-    "受注日を入力してください",
-    datetime.date(2021, 1, 1))
-day = d.strftime('%Y-%m-%d')
-#st.write(type(d))
-st.write('受注日:', day)
-
-#zaiko =df[df['製品番号']==select ,"在庫数"]
-zaiko =df[df['製品番号']==select ][['日付','在庫数']]
-zaiko = zaiko.set_index('日付')
-
-zai = df[(df['製品番号'] == select) & (df['日付']==day)]['在庫数'].iat[0]
-#st.write('現在の在庫数:',df[(df['製品番号'] == select) & (df['日付']==day)]['在庫数'])
-st.write('現在の在庫数:',zai)
-
-#st.line_chart(zaiko,x='日付',y='在庫数')
-st.line_chart(zaiko)
+with tab1:
+    st.header("受注")
 
 
-number = st.number_input('注文数を入力してください',step=1)
-st.write('The current number is ', number)
+    df = pd.read_csv("data.csv")
 
-if(number <= zai):
-    st.write('即納可能')
-else:
-    st.write('在庫不足')
+    parts_no = df["製品番号"].unique().tolist()
+
+    select = st.selectbox("部品番号", parts_no)
+
+    st.write('選択された製品は',select)
+
+    d = st.date_input(
+        "受注日を入力してください",
+        datetime.date(2021, 1, 1))
+    day = d.strftime('%Y-%m-%d')
+    st.write('受注日:', day)
+
+
+    zaiko =df[df['製品番号']==select ][['日付','在庫数']]
+    zaiko = zaiko.set_index('日付')
+
+    zai = df[(df['製品番号'] == select) & (df['日付']==day)]['在庫数'].iat[0]
+    st.write('現在の在庫数:',zai)
+
+
+    number = st.number_input('注文数を入力してください',step=1)
+    st.write('注文数: ', number)
+
+    if(number != 0):
+        if(number <= zai):
+            st.success('即納可能')
+        else:
+            st.error('在庫不足')
+
+    with st.expander("年間の在庫数推移(参考資料)"):
+        st.line_chart(zaiko)
+
+with tab2:
+    st.header("発注")
+
+
+with tab3:
+    st.header("棚卸")
