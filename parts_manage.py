04b43318372fdc0253e73b149935da42caeacf94
i-tosh <tosh.ishida@gmail.com>
2024-12-13 18:48:08 +0900

    発注機能を実装

--- parts_manage.py
+++ parts_manage.py
@@ -51,13 +51,13 @@ with tab1:
 
     select = st.selectbox("製品番号", parts_no, key="selectbox_tab1")
 
-    st.write('選択された製品は',select)
+    #st.write('選択された製品は',select)
 
     d = st.date_input(
         "受注日を入力してください",
         datetime.date(2021, 1, 1))
     day = d.strftime('%Y-%m-%d')
-    st.write('受注日:', day)
+    #st.write('受注日:', day)
 
     zaiko =st.session_state.df1[st.session_state.df1['製品番号']==select ][['日付','在庫数']]
     zaiko =zaiko.reset_index(drop=True)
@@ -69,7 +69,7 @@ with tab1:
     st.write('現在の在庫数:',zai)
 
     number = st.number_input('注文数を入力してください',step=1)
-    st.write('注文数: ', number)
+    #st.write('注文数: ', number)
 
     if(number != 0):
         if(number <= zai):
@@ -91,6 +91,44 @@ with tab2:
     st.header("発注")
 
 
+    # スタイル関数を定義
+    def highlight(row):
+        if row['在庫数'] <= row['発注点']:
+            return ['color: red', 'color: red']
+        else:
+            return ['', '']
+
+    parts_list = st.session_state.df1[st.session_state.df1["日付"]=='2021-01-01'].loc[:,['製品番号','発注点']]
+    #st.write(parts_list)
+
+    st.caption('本来は今日の日付を自動認識')
+    d2 = st.date_input(
+        "本日の日付を入力してください",
+        datetime.date(2021, 1, 1),key="selectbox_tab2")
+    day2 = d2.strftime('%Y-%m-%d')
+    #st.write('本日は', day2)
+    #在庫数一覧を取得
+    zaiko2 =st.session_state.df1[st.session_state.df1['日付']==day2][['製品番号','在庫数']]
+    #同じ日に複数の受注があるので、その場合は一番下の行の在庫数を採用する
+    zaiko2.reset_index( inplace=True,drop=True)
+    #st.write(zaiko2)
+        
+    grouped = zaiko2.groupby('製品番号').apply(lambda x: x.index[-1])
+    indices = grouped.tolist()
+    zaiko2 = zaiko2.iloc[[i  for i in indices]]
+    zaiko2 = pd.merge(zaiko2, parts_list)
+    zaiko2 = zaiko2.reset_index(drop=True)
+    st.divider()
+    st.write('発注すべき製品を赤字表示しています。また発注管理欄は1となっているので発注が必要なものを素早く確認する際には発注管理欄の並びを変えて下さい')
+    st.divider()
+    zaiko2['発注管理'] = 0
+    #発注が必要な場合は発注管理を1にする
+    zaiko2.loc[zaiko2['在庫数'] <= zaiko2['発注点'], '発注管理'] = 1
+    # DataFrameにスタイルを適用
+    styled_df = zaiko2.style.apply(highlight, axis=1, subset=['在庫数', '発注点'])
+    # Streamlitで表示
+    st.dataframe(styled_df, width=350, hide_index = True)
+    
 
 with tab3:
     st.header("棚卸")
@@ -109,14 +147,14 @@ with tab3:
 
     parts_no3 = st.session_state.df3["製品番号"].unique().tolist()
     select = st.selectbox("製品番号", parts_no3, key="selectbox_tab3")
-    st.write('選択された製品は',select)
+    #st.write('選択された製品は',select)
 
-    st.write('本来は今日の日付を自動認識')
+    st.caption('本来は今日の日付を自動認識')
     d = st.date_input(
         "本日の日付を入力してください",
         datetime.date(2021, 1, 1))
     day = d.strftime('%Y-%m-%d')
-    st.write('本日は', day)
+    #st.write('本日は', day)
 
     zaiko =st.session_state.df3[st.session_state.df3['製品番号']==select ][['日付','在庫数']]
     idx2 = zaiko[zaiko['日付']==day].index[0]   #棚卸の結果はidx2に上書きする
